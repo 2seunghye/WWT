e1bf6e1f3c2fed9b0d0f546ca0bc08ccceb6af29
2seunghye <tmdgp970120@naver.com>
2021-01-10 17:51:12 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { authService } from 'fBase';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (e) => {
     const {
@@ -15,8 +17,19 @@ const Auth = () => {
     }
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -24,7 +37,7 @@ const Auth = () => {
       <form onChange={onChange} onSubmit={onSubmit}>
         <input name="email" type="text" placeholder="Email" required value={email} />
         <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
       </form>
       <div>
         <button>Contivue with Google</button>
