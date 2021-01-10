6842b0b9ce1c1e46e3250b3dc16c93327f611a3c
2seunghye <tmdgp970120@naver.com>
2021-01-10 17:34:40 +0900

    Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onChange={onChange} onSubmit={onSubmit}>
+        <input name="email" type="text" placeholder="Email" required value={email} />
+        <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Contivue with Google</button>
+        <button>Contivue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
