2593af9ecb320f35334568c5eeb2eab0b63fedc4
2seunghye <tmdgp970120@naver.com>
2021-01-10 18:18:17 +0900

    Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,18 +5,17 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
-
-  const onChange = (e) => {
+  const [error, setError] = useState('');
+  const onChange = (event) => {
     const {
       target: { name, value },
-    } = e;
+    } = event;
     if (name === 'email') {
       setEmail(value);
     } else if (name === 'password') {
       setPassword(value);
     }
   };
-
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -28,20 +27,23 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
-      <form onChange={onChange} onSubmit={onSubmit}>
-        <input name="email" type="text" placeholder="Email" required value={email} />
-        <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
-        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+      <form onSubmit={onSubmit}>
+        <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Sign In'} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? 'Sign In' : 'Create Account'}</span>
       <div>
-        <button>Contivue with Google</button>
-        <button>Contivue with Github</button>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
       </div>
     </div>
   );
