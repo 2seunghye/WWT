eb7101926ff3974b7b11891268204f6e994ea0b9
2seunghye <tmdgp970120@naver.com>
2021-01-10 22:47:54 +0900

    Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [feed, setFeed] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setFeed(value);
+  };
+  return (
+    <div>
+      <form>
+        <input
+          value={feed}
+          onChange={onChange}
+          type="text"
+          placeholder="
+    What are you wearing today?"
+          maxLength={120}
+        />
+        <input onClick={onSubmit} type="submit" value="share" />
+      </form>
+    </div>
+  );
+};
 export default Home;
