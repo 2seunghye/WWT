e4f2c2c4e0fabb32962fda6345f2252b4ed6a145
2seunghye <tmdgp970120@naver.com>
2021-01-10 23:31:47 +0900

    Getting the Feeds

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { dbService } from 'fBase';
 
 const Home = () => {
   const [feed, setFeed] = useState('');
+  const [feeds, setFeeds] = useState([]);
+  const getFeeds = async () => {
+    const dbFeeds = await dbService.collection('feeds').get();
+    dbFeeds.forEach((document) => {
+      const feedObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setFeeds((prev) => [feedObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getFeeds();
+  }, []);
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await dbService.collection('feeds').add({
@@ -18,6 +33,7 @@ const Home = () => {
     } = e;
     setFeed(value);
   };
+  console.log(feeds);
   return (
     <div>
       <form>
@@ -31,6 +47,13 @@ const Home = () => {
         />
         <input onClick={onSubmit} type="submit" value="share" />
       </form>
+      <div>
+        {feeds.map((feed) => (
+          <div key={feed.id}>
+            <h4>{feed.feed}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
